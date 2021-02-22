f1a5510af8b24f9365c23605d0d79821be0f491f
율링 <76838077+rimiiii@users.noreply.github.com>
2021-02-22 22:07:17 +0900

    Create basic-print.py

---
+++ CodeUpBasic100/basic-print.py
@@ -0,0 +1,23 @@
+# 6001
+print('Hello')
+
+# 6002
+print('Hello World')
+
+# 6003
+print('Hello\nWorld')
+
+# 6004
+print("'Hello'")
+
+# 6005
+print('"Hello World"')
+
+# 6006
+print('"!@#$%^&*()\'')
+
+# 6007
+print('''"C:\Download\\'hello'.py"''')
+
+# 6008
+print('print("Hello\\nWorld")')
