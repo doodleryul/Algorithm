7eeb89c3dd11b3e1a88dd93c3443852929f52611
율링 <76838077+rimiiii@users.noreply.github.com>
2021-02-24 23:20:30 +0900

    Create basic_inputOutput.py

---
+++ CodeUpBasic100/basic_inputOutput.py
@@ -0,0 +1,73 @@
+# 6009
+x = input()
+print(x)
+
+# 6010
+x = input()
+print(x)
+
+# 6011
+x = input()
+print(x)
+
+# 6012
+a = input() 
+b = input()
+print(a)
+print(b)
+
+# 6013
+a = input() 
+b = input()
+print(b)
+print(a)
+
+# 6014
+f = input()
+print(f)
+print(f)
+print(f)
+
+# 6015
+a, b = input().split()
+print(a)
+print(b)
+
+# 6016
+a, b = input().split()
+print(b)
+print(a)
+
+# 6017
+s = input()
+print(s, s, s)
+
+# 6018
+a, b = input().split(':')
+print(a, b, sep=':')
+
+# 6019
+a, b, c = input().split('.')
+print(c, b, a, sep='-')
+
+# 6020
+a, b = input().split('-')
+print(a, b, sep='')
+
+# 6021
+s = input()
+for i in s:
+  print(i)
+
+# 6022
+s = input()
+print(s[0:2], s[2:4], s[4:6])
+
+# 6023
+h, m, s = input().split(':')
+print(m)
+
+# 6024
+w1, w2 = input().split()
+s = w1 + w2
+print(s)
