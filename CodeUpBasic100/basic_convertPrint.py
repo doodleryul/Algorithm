51c12ed09367139e8328fb3303d67070208fb253
율링 <76838077+rimiiii@users.noreply.github.com>
2021-03-02 23:49:15 +0900

    Create basic_convertPrint.py

---
+++ CodeUpBasic100/basic_convertPrint.py
@@ -0,0 +1,11 @@
+#%x: 16진수, %X: 진수 대문자
+
+#6027 10진수를 입력받아 16진수(hexadecimal)로 출력해보자. 
+a = input()
+n = int(a)
+print('%x'%n)
+
+#6028 10진수를 입력받아 16진수(hexadecimal)로 출력해보자.(대문자 형태 문자열로 출력)
+a = input()
+n = int(a)
+print('%X'%n)
