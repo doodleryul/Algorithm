8ed4de74708935d77d2a239b3886be643a1793c0
율링 <76838077+rimiiii@users.noreply.github.com>
2021-03-13 00:58:36 +0900

    Update basic_arithmetic_operation.py

--- CodeUpBasic100/basic_arithmetic_operation.py
+++ CodeUpBasic100/basic_arithmetic_operation.py
@@ -46,3 +46,27 @@ print(int(a)//int(b))
 #6041 정수 2개(a, b) 를 입력받아 a를 b로 나눈 나머지를 출력해보자. 
 a, b = input().split()
 print(int(a)%int(b))
+
+#6043 실수 2개(f1, f2)를 입력받아 f1 을 f2 로 나눈 값을 출력해보자. 이 때 소숫점 넷째자리에서 반올림하여 무조건 소숫점 셋째 자리까지 출력한다.
+f1, f2 = input().split()
+result = round(float(f1)/float(f2), 3)
+print('{:.3f}'.format(result))
+
+#6044 정수 2개(a, b)를 입력받아 합, 차, 곱, 몫, 나머지, 나눈 값을 자동으로 계산해보자.
+#단 0 <= a, b <= 2147483647, b는 0이 아니다.
+x, y = input().split()
+a = int(x)
+b = int(y)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(round(a/b, 2))
+
+#6045 정수 3개를 입력받아 합과 평균을 출력해보자.
+#단, -2147483648 ~ +2147483647
+a, b, c = input().split()
+result_sum = int(a)+int(b)+int(c)
+result_avg = round(result_sum/3, 2)
+print(result_sum, result_avg)
